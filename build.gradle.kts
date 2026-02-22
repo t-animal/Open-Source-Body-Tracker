f6ade9ba607296b78b6c42d5ace23a78396a725d
t.animal <tilman-git@t-animal.de>
2026-02-22 20:11:47 +0100

    Add ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,25 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+import org.jlleitschuh.gradle.ktlint.KtlintExtension
+
 plugins {
     alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.ktlint) apply false
+}
+
+allprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    extensions.configure<KtlintExtension> {
+        outputToConsole.set(true)
+        ignoreFailures.set(false)
+
+        filter {
+            exclude("**/build/**")
+            exclude("**/generated/**")
+        }
+    }
+
+    tasks.matching { it.name == "check" }.configureEach {
+        dependsOn("ktlintCheck")
+    }
 }
