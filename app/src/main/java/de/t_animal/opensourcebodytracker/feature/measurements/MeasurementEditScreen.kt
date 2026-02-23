c921a9889fa1ad8b8df42fab9f652a98999b693a
Tilman Adler <tilman-git@t-animal.de>
2026-02-23 15:54:13 +0100

    Handle keyboard insets on measurement form

--- app/src/main/java/de/t_animal/opensourcebodytracker/feature/measurements/MeasurementEditScreen.kt
+++ app/src/main/java/de/t_animal/opensourcebodytracker/feature/measurements/MeasurementEditScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -103,6 +104,7 @@ fun MeasurementEditScreen(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(padding)
+                .imePadding()
                 .verticalScroll(scrollState)
                 .padding(16.dp),
         ) {
